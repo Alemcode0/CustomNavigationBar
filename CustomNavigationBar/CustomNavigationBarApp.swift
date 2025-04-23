f540bdc8a995316cd99debd7e7f5f6a3d075da1a
alemayehu <alemcode0@gmail.com>
2025-04-23 13:24:24 +0200

    Initial Commit

---
+++ CustomNavigationBar/CustomNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationBarApp.swift
+//  CustomNavigationBar
+//
+//  Created by Alemayehu Hailye on 23.04.25.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
